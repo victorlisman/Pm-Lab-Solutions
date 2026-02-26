8c4111e323b0527448c94500f2e0d4a50e06060d
Victor Lisman <victor.lisman@protonmail.com>
2026-02-26 10:52:48 +0200

    Ex2 fix

--- Lab1/src/bin/ex2.rs
+++ Lab1/src/bin/ex2.rs
@@ -1,9 +1,8 @@
 fn n_odd(n: u32) {
     for i in 0..n {
-        if i % 2 == 1 {
-            println!("{}", i);
-        }
-    }
+        let odd = 2 * i + 1;
+        println!("{}", odd);
+    } 
 }
 
 fn main() {
